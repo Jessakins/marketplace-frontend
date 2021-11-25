0022be768b4ef71fcdd46b529d5193c81d8b1f0e
Jessakins <janessamaeburila22@gmail.com>
2021-11-25 13:31:53 +0800

    commit on master

---
+++ src/components/Products.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+const Products = [
+    {
+        id: 1, 
+        name: 'Yellowfin Tuna',
+        price: 'P380.00/kilo',
+        thumb: "./img/fish1.jpg"
+    },
+    {
+        id: 2, 
+        name: 'Skipjack Tuna',
+        price: 'P603.50/kilo',
+        thumb: "./img/fish2.jpg"
+    },
+    {
+        id: 3, 
+        name: 'Yellowfin Tuna',
+        price: 'P380.00/kilo',
+        thumb: "./img/fish3.jpg"
+    },
+    {
+        id: 4, 
+        name: 'Skipjack Tuna',
+        price: 'P710.00/kilo',
+        thumb: "./img/fish4.jpg"
+    },
+    
+];
+
+export default Products;
