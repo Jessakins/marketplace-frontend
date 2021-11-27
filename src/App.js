a0bfc038f4e75091a13e718afdeb535d083db787
Jessakins <janessamaeburila22@gmail.com>
2021-11-27 09:59:49 +0800

    commit on master

--- src/App.js
+++ src/App.js
@@ -7,19 +7,22 @@ function App() {
     <div className="App">
       <Header/>
       
-      <div class="container flex-row">
-        <h2 class= "text-blue font-montserrat font-bold text-xl ml-60 pt-5">Browse Products</h2>
-        <h3 class= "text-black text-xs font-montserrat ml-60">Home / Products</h3>
+      <div class=" pt-3 flex justify-between">
+        <header>
+          <h2 class= "text-blue font-montserrat font-bold text-xl ml-60 pt-2">Browse Products</h2>
+          <h3 class= "text-black text-xs font-montserrat ml-60">Home / Products</h3>
+        </header>
 
-        <button class="text-black text-xs font-montserrat rounded-full border-blue border-2 p-3">
-          Search for products... <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 " fill="none" 
-          viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" 
+        <button class="text-black text-xs flex font-montserrat rounded-full border-blue border-2 pt-4 px-5 mr-20">
+          Search for products... <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 ml-20" fill="none" 
+          viewBox="0 0 24 24" stroke="Blue"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" 
           d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"/></svg>
         </button>
-      
-      <Products/>
       </div>
 
+    <Products/>
+
+    
     </div>
   );
 }
