a70cfd08b4fc4d2818eac2be96a4c7e0dc5a0570
Jessakins <janessamaeburila22@gmail.com>
2021-11-27 10:00:08 +0800

    master

--- src/components/Header.js
+++ src/components/Header.js
@@ -13,7 +13,7 @@ export default function Header () {
                         <button class="text-blue text-2xl font-bold rounded-full border-blue border-2 hover:text-white 
                         hover:bg-blue hover:border-transparent font-montserrat px-5 ml-auto mr-5">Log In</button>
                     
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-15 w-12 px-2 py-2 rounded-full border-blue border-2" fill="none" viewBox="0 0 24 24" stroke="blue"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="1" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" /></svg>
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-15 w-20 px-4 py-4 rounded-full border-blue border-2" fill="none" viewBox="0 0 24 24" stroke="blue"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="1" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" /></svg>
                     </div>
                 </div>
             </div>
--- src/components/Products.js
+++ src/components/Products.js
@@ -1,30 +1,39 @@
-import React from 'react'
+import React, { useState, useEffect } from "react"; 
 
-const Products = [
-    {
-        id: 1,
-        name: 'Yellowfin Tuna',
-        price: '₱380.00/kilo',
-        thumb: './img/1.jpg'
-    },
-    {
-        id: 2,
-        name: 'Skipjack Tuna',
-        price: '₱603.50/kilo',
-        thumb: './img/2.jpg'
-    },
-    {
-        id: 3,
-        name: 'Yellowfin Tuna',
-        price: '₱380.00/kilo',
-        thumb: './img/3.jpg'
-    },
-    {
-        id: 4,
-        name: 'Skipjack Tuna',
-        price: '₱710.00/kilo',
-        thumb: './img/4.jpg'
-    }
-]
+function Products (){
+    
+
+    const [products, setProducts] = useState([
+        {
+            id: 1,
+            name: 'Yellowfin Tuna',
+            price: '₱380.00/kilo',
+            thumb: './img/1.jpg'
+        },
+        {
+            id: 2,
+            name: 'Skipjack Tuna',
+            price: '₱380.00/kilo',
+            thumb: './img/1.jpg'
+        },
+        {
+            id: 3,
+            name: 'Yellowfin Tuna',
+            price: '₱380.00/kilo',
+            thumb: './img/1.jpg'
+        },
+        {
+            id: 4,
+            name: 'Skipjack Tuna',
+            price: '₱380.00/kilo',
+            thumb: './img/1.jpg'
+        },
+    ]);
+
+    return (
+        <>
+         <div>{products.map(prod => <div key={prod.name}> {prod.name} </div>)}</div>
+        </>)
+}
 
 export default Products;
